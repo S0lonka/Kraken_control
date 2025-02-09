be2cfa5ccc96ee645a8aacf3924a02019d514473
Solonka <solowayplay@gmail.com>
2025-02-09 20:34:53 +0300

    изменил пути к файлам

--- app/interfaces/start_interface.py
+++ app/interfaces/start_interface.py
@@ -9,7 +9,7 @@ from qasync import asyncSlot, QEventLoop
 
 # Импорты моих библиотек
 from main_interface import MainWindow
-from utils.license_interface import LicenseAgreementDialog
+from interfaces.license_interface import LicenseAgreementDialog
 
 
 #! Класс Стартового окна
